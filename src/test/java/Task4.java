7c411ec10bb367fe005f2aad0702d8551288f3a9
Ana Khurtsidze <anakhurtsidze@credo.ge>
2024-11-03 14:58:38 +0400

    fix in task4_ana

--- src/test/java/Task4.java
+++ src/test/java/Task4.java
@@ -1,18 +1,20 @@
+
+
 public class Task4 {
     public static void main(String[] args) {
-        String stringValue = null ;
         int [] array = {45, 46, 47, 48, 49, 50, 51, 52};
+        String stringValue = null ;
+
 
         try {
+            System.out.println("Array index is out of bounds.");
             System.out.println(stringValue.length());
-        }catch (NullPointerException e){
-            System.out.println("String variable is null.");
-        }
-
-        try {
-            System.out.println(array[12]);
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Array index is out of bounds.");
         }
+        catch (NullPointerException e){
+            System.out.println("String variable is null.");
+        }
+
     }
 }
